2ea195480286ca488ccacc6b7af04e1b8e584198
Ashik Ghosh <ashikghosh763@gmail.com>
2022-06-24 19:04:44 +0600

    idea pailam

--- ArrayAndAll/src/ArrayAndAll.java
+++ ArrayAndAll/src/ArrayAndAll.java
@@ -1,21 +1,45 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayAndAll {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 
-        //Array of Primitives
-        int[] name = new int[5];
-        for(int i=0;i<5;i++){
-            name[i]= input.nextInt();
-        }
-        for (int val:name) {
-            System.out.print(val + " ");
+//        //Array of Primitives
+//        int[] name = new int[5];
+//        System.out.println(name.length);
+//        for(int i=0;i<5;i++){
+//            name[i]= input.nextInt();
+//        }
+//        for (int val:name) {
+//            System.out.print(val + " ");
+//
+//        }
+////        System.out.println(Arrays.toString(name));
+//
+//        // Array of non-primitive
+//
+//        String[] str = new String[2];
+//
+////   or,String[] str = {"hello","world"};
+//
+//        for(int i = 0;i<2;i++){
+//            str[i] = input.next();
+//        }
+//
+////        for(String val:str){
+////            System.out.println(val);
+////
+////        }
+////        or,
+//        for(int i=0;i<2;i++){
+//            System.out.println(str[i]);
+//
+//        }
 
-        }
-//        System.out.println(Arrays.toString(name));
-//        String[]
+        //String
+
+        String hello = input.next();
+        System.out.println(hello);
 
 
     }
