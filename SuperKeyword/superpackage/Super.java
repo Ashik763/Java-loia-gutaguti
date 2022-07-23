353c424ad3a875a36b1234598c4718a738393fce
Ashik Ghosh <ashikghosh763@gmail.com>
2022-07-24 00:05:10 +0600

    super keyword

---
+++ SuperKeyword/superpackage/Super.java
@@ -0,0 +1,9 @@
+package superpackage;
+
+public class Super {
+    public static void main(String[] args) {
+        SubClass obj = new SubClass();
+
+    }
+
+}
---
+++ SuperKeyword/superpackage/SuperClass.java
@@ -0,0 +1,9 @@
+package superpackage;
+
+public class SuperClass {
+    String S = "From Super";
+    SuperClass(){
+        System.out.println(S);
+    }
+
+}
